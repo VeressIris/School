65401b0dfd8a852d58b5170b6997cf25b6ce91c3
VeressIris <68661896+VeressIris@users.noreply.github.com>
2021-11-14 18:50:52 +0200

    Add files via upload

---
+++ zero_unu.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    bool check_1 = false;
+    bool check_0 = false;
+    bool check_alt = false;
+    int cifra;
+    while (n != 0)
+    {
+        cifra = n % 10;
+
+        if (cifra != 1 && cifra != 0)
+        {
+            check_alt = false;
+            check_0 = false;
+            check_1 = false;
+            break;
+        }
+        else if (cifra == 1)
+        {
+            check_1 = true;
+        }
+        else if (cifra == 0)
+        {
+            check_0 = true;
+        }
+
+        n /= 10;
+    }
+
+    if (check_1 == true && check_0 == true && check_alt == false)
+    {
+        cout << "da";
+    }
+    else
+    {
+        cout << "nu";
+    }
+
+    return 0;
+}
