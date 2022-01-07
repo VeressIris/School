4bb71e0fd9796038bcfd0bde1bff3265f73966dd
VeressIris <68661896+VeressIris@users.noreply.github.com>
2022-01-07 08:17:27 +0200

    Create SumaPrima.cpp

---
+++ SumaPrima.cpp
@@ -0,0 +1,41 @@
+//se citeste un nr nat x verificati daca suma cifrelor lui e un nr prim
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    int sum = 0;
+    int cif;
+    while(x != 0)
+    {
+        cif = x % 10;
+        sum += cif;
+        x /= 10;
+    }
+
+    int prim = 1;
+    for (int i = 2; i <= sum / 2; i++)
+    {
+        if (sum % i == 0)
+        {
+            prim = 0;
+        }
+    }
+
+    if (prim == 1)
+    {
+        cout << "suma cifrelor este prima";
+    }
+    else
+    {
+        cout << "suma cifrelor nu este prima";
+
+    }
+
+    return 0;
+}
