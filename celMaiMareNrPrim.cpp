6450bebefa48464e82ed6d50751f97f24d4fd015
VeressIris <68661896+VeressIris@users.noreply.github.com>
2022-01-19 11:43:57 +0200

    Create celMaiMareNrPrim.cpp

---
+++ celMaiMareNrPrim.cpp
@@ -0,0 +1,41 @@
+//Se citesc n numere, afisati cel mai mare numar prim citit.
+
+#include <iostream>
+#include <climits> //INT_MIN
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int numar;
+    int maxim = INT_MIN;
+
+    bool prim = true;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> numar;
+
+        for (int j = 2; j * j <= numar; j++)
+        {
+            if (numar % j == 0)
+            {
+                prim = false;
+                break;
+            }
+        }
+
+        if (prim == true && numar > maxim)
+        {
+            maxim = numar;
+        }
+        cout << "maxim: " << maxim << endl;
+    }
+
+    cout << maxim;
+
+    return 0;
+}
